433ee99c55fe8d449cbe12aa56cc3faa427f3ea6
sonups <sonu.sadasivan@gmail.com>
2021-05-27 11:03:40 +0530

    added support for Remote GRID execution

--- src/main/java/automation/core/DriverFactory.java
+++ src/main/java/automation/core/DriverFactory.java
@@ -4,8 +4,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverFactory {
 
@@ -32,6 +36,21 @@ public class DriverFactory {
             tlDriver.set(new FirefoxDriver());
         } else if (browser.equals("safari")) {
             tlDriver.set(new SafariDriver());
+        } else if (browser.equals("remote")) {
+            String ip = System.getenv("SELENIUM_HUB_ADDRESS");
+            String hubURL = new String();
+            if(ip !=null)
+                hubURL = "http://"+ip+":4444/wd/hub";
+            else
+                hubURL = "http://localhost:4444/wd/hub";
+            DesiredCapabilities capability = DesiredCapabilities.chrome();
+            RemoteWebDriver driver = null;
+            try {
+                driver = new RemoteWebDriver(new URL(hubURL), capability);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            tlDriver.set(driver);
         } else {
             System.out.println("Please pass the correct browser value: " + browser);
         }
